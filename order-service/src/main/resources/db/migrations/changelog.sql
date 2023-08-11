3c47fb52767eded9385b3156ddca78baef47a3e3
t4pusd <sdtapusd20@gmail.com>
2023-08-12 00:13:57 +0600

    changed quantity type from float to int

--- order-service/src/main/resources/db/migrations/changelog.sql
+++ order-service/src/main/resources/db/migrations/changelog.sql
@@ -26,3 +26,9 @@ CREATE TABLE "order_product" (
 -- precondition-sql-check expectedResult:0 select count(*) from information_schema.columns where table_name = 'order' and column_name = 'customer_email';
 ALTER TABLE "order" ADD customer_name varchar(150) NOT NULL DEFAULT '',
     ADD customer_email varchar(255) NOT NULL DEFAULT '';
+
+-- changeset tapusd:order-change-order-product-quantity-to-integer-from-float
+-- preconditions onFail:MARK_RAN onError:MARK_RAN
+-- precondition-sql-check expectedResult:1 select count(*) from information_schema.tables where table_name = 'order_product';
+-- precondition-sql-check expectedResult:1 select count(*) from information_schema.columns where table_name = 'order_product' and column_name = 'quantity';
+ALTER TABLE "order_product" ALTER COLUMN quantity TYPE int;
