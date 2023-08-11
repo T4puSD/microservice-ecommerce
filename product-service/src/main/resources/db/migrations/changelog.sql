cb9e6105cb4e5e8d729e572ceda00e66106e5f1d
t4pusd <sdtapusd20@gmail.com>
2023-08-11 23:14:49 +0600

    fixed preconditions

--- product-service/src/main/resources/db/migrations/changelog.sql
+++ product-service/src/main/resources/db/migrations/changelog.sql
@@ -15,16 +15,20 @@ INSERT INTO product (name) VALUES
     ('Samsung Galaxy 23 Ultra');
 
 -- changeset tapusd:product-price-field-add
+-- preconditions onFail:MARK_RAN onError:MARK_RAN
 -- precondition-sql-check expectedResult:1 select count(*) from information_schema.tables where table_name = 'product';
+-- precondition-sql-check expectedResult:0 select count(*) from information_schema.columns where table_name = 'product' and column_name = 'price';
 ALTER TABLE "product" ADD COLUMN price float NOT NULL DEFAULT 0.0;
 
 -- changeset tapusd:product-price-field-add-update-init-data
+-- preconditions onFail:MARK_RAN onError:MARK_RAN
 -- precondition-sql-check expectedResult:3 select count(*) from product;
 UPDATE "product" set price = 199.0 WHERE name = 'Iphone 12s';
 UPDATE "product" set price = 399.0 WHERE name = 'Iphone 13 Pro';
 UPDATE "product" set price = 250.0 WHERE name = 'Samsung Galaxy 23 Ultra';
 
 -- changeset tapusd:product-name-unique
--- precondition-sql-check expectedResult:1 select count(*) from information_schema.tables where table_name = 'product';
+-- preconditions onFail:MARK_RAN onError:MARK_RAN
+-- precondition-sql-check expectedResult:0 select * from information_schema.table_constraints where table_name = 'product' and constraint_name = 'unique_product_name';
 ALTER TABLE "product" ADD CONSTRAINT "unique_product_name" UNIQUE (name);
 --rollback alter table "product" drop constraint "unique-product-name";
