d2012de57176999835102b6b6c55e9dc3b0e3dbb
t4pusd <sdtapusd20@gmail.com>
2023-08-13 15:18:58 +0600

    added a request logging filter

---
+++ product-service/src/main/java/com/tapusd/productservice/filters/LoggingFilter.java
@@ -0,0 +1,26 @@
+package com.tapusd.productservice.filters;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class LoggingFilter implements Filter {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(LoggingFilter.class);
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest)  servletRequest;
+        LOGGER.debug("Requested URI: {}", req.getRequestURI());
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+}
