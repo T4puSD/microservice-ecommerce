15e266fed73a3ea2108d829d30da43ab9d594c40
t4pusd <sdtapusd20@gmail.com>
2023-08-27 21:19:37 +0600

    returned mono instead of response entity

--- gateway/src/main/java/com/tapusd/gateway/fallback/FallbackController.java
+++ gateway/src/main/java/com/tapusd/gateway/fallback/FallbackController.java
@@ -2,19 +2,18 @@ package com.tapusd.gateway.fallback;
 
 import com.tapusd.gateway.dto.Response;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("/fallback")
 public class FallbackController {
 
     @GetMapping("/todos")
-    public ResponseEntity<Response> fallbackTodoResponse() {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                .body(new Response()
+    public Mono<Response> fallbackTodoResponse() {
+        return Mono.just(new Response()
                         .setStatus(HttpStatus.NOT_FOUND.value())
                         .setMessage("Service Not Found!")
                 );
