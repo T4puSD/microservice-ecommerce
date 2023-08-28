a18b2a553842652f073f1f20f763c23c2ca44b85
t4pusd <sdtapusd20@gmail.com>
2023-08-28 09:15:48 +0600

    added custom time limiter circuitbreaker config

---
+++ gateway/src/main/java/com/tapusd/gateway/config/Resilience4JConfig.java
@@ -0,0 +1,28 @@
+package com.tapusd.gateway.config;
+
+import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
+import io.github.resilience4j.timelimiter.TimeLimiterConfig;
+import org.springframework.cloud.circuitbreaker.resilience4j.ReactiveResilience4JCircuitBreakerFactory;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder;
+import org.springframework.cloud.client.circuitbreaker.Customizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.Duration;
+
+@Configuration
+public class Resilience4JConfig {
+
+    @Bean
+    public Customizer<ReactiveResilience4JCircuitBreakerFactory> defaultCustomizer() {
+        return factory -> factory.configureDefault(id -> new Resilience4JConfigBuilder(id)
+                .circuitBreakerConfig(CircuitBreakerConfig.ofDefaults())
+                .timeLimiterConfig(
+                        TimeLimiterConfig.custom()
+                                // todos endpoint will alwasy fail because of 200ms config
+                                .timeoutDuration(Duration.ofMillis(200))
+                                .build()
+                )
+                .build());
+    }
+}
